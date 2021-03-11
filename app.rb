94d0aa12239c154c5b2cecfbcbbdeaf53f03057a
Aman Tank <amantank187@gmail.com>
2021-03-11 12:53:37 +0000

    Able to create a user

--- app.rb
+++ app.rb
@@ -2,14 +2,15 @@ require 'sinatra/base'
 require './lib/peep'
 require './database_connection_setup'
 require 'time'
+require './lib/user'
 
 class ChitterApp < Sinatra::Base 
     enable :sessions
     
     get '/' do
+        @user = User.find(session[:user_id])
           @peeps = Peep.all
-          erb :"peeps/index" 
-          
+          erb :"peeps/index"  
       end
 
     get '/peeps/new' do
@@ -21,6 +22,17 @@ class ChitterApp < Sinatra::Base
         Peep.create(message: params[:message])
         redirect '/'
     end 
+
+    get '/users/new' do
+
+        erb:'/users/new'
+    end 
+
+    post '/users' do
+        User.create(email: params['email'], password: params['password'])
+        session[:user_id] = user.id
+        redirect '/'
+    end
 
     run! if app_file == $0 
 end 
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,5 +4,5 @@ def setup_test_database
 
     connection = PG.connect(dbname: 'chitter_app_test')
 
-    connection.exec("TRUNCATE peeps;")
+    connection.exec("TRUNCATE peeps, users;")
 end 
